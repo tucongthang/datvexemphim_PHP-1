623c076f6671151e9544a81a2b82970d0b018a4f
zalaphng <tin.nt.26oct02@gmail.com>
2023-11-15 10:20:47 +0700

    get admin commit

--- admin/exec/screens.php
+++ admin/exec/screens.php
@@ -8,17 +8,23 @@ if (isset($_POST['add-screen-btn'])) {
     $theater_id = mysqli_real_escape_string($conn, $_POST['theater-id']);
     $screen_number = mysqli_real_escape_string($conn, $_POST['screen-number']);
 
-    $insert_record = mysqli_query($conn, "INSERT INTO screens (`theater_id`, `screen_number`) VALUES ('" . $theater_id . "','" . $screen_number . "')");
+    try {
 
-    if (!$insert_record) {
-        $msg = "Insert unsuccessful";
+        $insert_record = mysqli_query($conn, "INSERT INTO screens (`theater_id`, `screen_number`) VALUES ('" . $theater_id . "','" . $screen_number . "')");
+
+        if ($insert_record) {
+            $msg = "Insert successful";
+            $error = 0;
+        } else {
+            throw new Exception("Insert unsuccessful");
+        }
+    } catch (Exception $e) {
+        $msg = "An error occurred: " . $e->getMessage();
         $error = 1;
-    } else {
-        $msg = "Insert successful";
-        $error = 0;
-        header("Location: ../screens.php");
-        exit();
     }
+
+    header("Location: ../screens.php?msg=" . urlencode($msg) . "&error=" . $error);
+    exit();
 }
 
 if (isset($_POST['update-screen-btn'])) {
@@ -26,31 +32,43 @@ if (isset($_POST['update-screen-btn'])) {
     $edit_theater_id = mysqli_real_escape_string($conn, $_POST['edit-theater-id']);
     $edit_screen_number = mysqli_real_escape_string($conn, $_POST['edit-screen-number']);
 
-    $update_record = mysqli_query($conn, "UPDATE `screens` SET `theater_id` = '$edit_theater_id', `screen_number` = '$edit_screen_number' WHERE `id` = '$e_id'");
 
-    if (!$update_record) {
-        $msg = "Update unsuccessful";
+    try {
+        $update_record = mysqli_query($conn, "UPDATE `screens` SET `theater_id` = '$edit_theater_id', `screen_number` = '$edit_screen_number' WHERE `id` = '$e_id'");
+
+        if ($update_record) {
+            $msg = "Update successful";
+            $error = 0;
+        } else {
+            throw new Exception("Update unsuccessful");
+        }
+    } catch (Exception $e) {
+        $msg = "An error occurred: " . $e->getMessage();
         $error = 1;
-    } else {
-        $msg = "Update successful";
-        $error = 0;
-        header("Location: ../screens.php");
-        exit();
     }
+
+    header("Location: ../screens.php?msg=" . urlencode($msg) . "&error=" . $error);
+    exit();
 }
 
 if (isset($_POST['delete-screen-btn'])) {
     $id = mysqli_real_escape_string($conn, $_POST['id']);
 
-    $delete_record = mysqli_query($conn, "DELETE FROM screens WHERE id=$id");
 
-    if (!$delete_record) {
-        $msg = "Delete unsuccessful";
+    try {
+        $delete_record = mysqli_query($conn, "DELETE FROM screens WHERE id=$id");
+
+        if ($delete_record) {
+            $msg = "Delete successful";
+            $error = 0;
+        } else {
+            throw new Exception("Delete unsuccessful");
+        }
+    } catch (Exception $e) {
+        $msg = "An error occurred: " . $e->getMessage();
         $error = 1;
-    } else {
-        $msg = "Delete successful";
-        $error = 0;
-        header("Location: ../screens.php");
-        exit();
     }
+
+    header("Location: ../screens.php?msg=" . urlencode($msg) . "&error=" . $error);
+    exit();
 }
